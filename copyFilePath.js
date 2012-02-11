78cc338fc14e921f9322e1b71d3ab5499ec1aca3
Yuta Inage <yuta.post@gmail.com>
2012-02-11 17:32:39 +0900

    refactoring

--- copyFilePath.js
+++ copyFilePath.js
@@ -2,13 +2,13 @@ function copyfilePath(e) {
   if (e.target.tagName == "A") {
     var hrefAttribute = e.target.getAttribute('href');
     if (hrefAttribute.indexOf("file:") == 0) {
-      var filePath = hrefAttribute.substr(5);
-      filePath = filePath.replace("///", "");
-      filePath = filePath.replace(/\//g, "\\");
-      chrome.extension.sendRequest({path: filePath}, function(response) {
+      console.log(hrefAttribute);
+      chrome.extension.sendRequest({url: hrefAttribute}, function(response) {
         // console.log("send");
       });
     }
   }
 }
+
 document.addEventListener("dblclick", copyfilePath, true);
+
